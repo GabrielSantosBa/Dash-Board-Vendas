f348461fed622b8a95f347c68ed3c14644f2f9d0
GabrielSantosBa <gab.arteinfo@gmail.com>
2021-05-03 19:40:16 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/dsvendas/DashBoardVendas/DashBoardVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsvendas.DashBoardVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashBoardVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
