9f86849fda0a92ea7adcdbf32822810ddb293127
GabrielSantosBa <gab.arteinfo@gmail.com>
2021-05-06 19:09:54 -0300

    Estrutura basica de forma estatica ainda

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
- 
-
-
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+import NavBar from "components/index";
 
 function App() {
- return(
-  <div>
-    <h1 className="text-primary">olá Mundo</h1>
-  </div>
+  return (
 
- );
- 
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">olá Mundo</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
+  );
+
 }
 
 export default App;
