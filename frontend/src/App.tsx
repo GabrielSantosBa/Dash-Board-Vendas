f416a46d60bdde145d91464c7a733c1076526ec4
GabrielSantosBa <gab.arteinfo@gmail.com>
2021-05-06 19:40:58 -0300

    Graficos Implementados via apecharts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import Footer from "components/footer";
+import GraficoRosca from "components/GraficoRosca";
 import NavBar from "components/index";
 
 function App() {
@@ -8,9 +10,33 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">olá Mundo</h1>
+        <h1 className="text-primary py-3">Dash Vendas</h1>
+
+
+        <div className="row">
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Porcentagem de Vendas ( % )</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Porcentagem de Vendas ( % )</h5>
+            <GraficoRosca />
+          </div>
+
+
+        </div>
+
+
+
+        <div className="py-3">
+          <h2 className="text-primary text-center">Todas as vendas</h2>
+        </div>
+        <DataTable />
       </div>
-      <DataTable />
+
+
+
       <Footer />
     </>
   );
