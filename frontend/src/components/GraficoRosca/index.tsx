bdf29af0af26c692ec13f42813fc02898a7e7924
GabrielSantosBa <gab.arteinfo@gmail.com>
2021-05-09 13:43:11 -0300

    Grafico de Rosca

--- frontend/src/components/GraficoRosca/index.tsx
+++ frontend/src/components/GraficoRosca/index.tsx
@@ -1,12 +1,30 @@
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import Chart from 'react-apexcharts';
+import { VendaSoma } from 'types/sales';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 function GraficoRosca() {
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
+
+useEffect(()=> {
+    axios.get(`${BASE_URL}/sales/soma-por-vendedor`)
+    .then(response => {
+        const data = response.data as VendaSoma[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        setChartData ({ labels: myLabels, series: mySeries });
+    });
+}, []);
     
     const options = {
         legend: {
@@ -15,14 +33,12 @@ function GraficoRosca() {
     }
 
     return (
-        <Chart 
-
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
-            />
-
+        />
     );
 
 }
---
+++ frontend/src/types/sales.ts
@@ -0,0 +1,6 @@
+export type VendaSoma = {
+
+    sellerName: string;
+    sum: number;
+}
+
